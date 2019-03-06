43f5cbeb94f9a9b7162c6f77c1a66a9f4f40bd9e
zhengqigo <zhengqigo@vip.163.com>
2019-03-06 14:17:19 +0800

    tijiao

---
+++ src/main/java/org/fuelteam/watt/test/tuple/DefaultTupleTest.java
@@ -0,0 +1,70 @@
+package org.fuelteam.watt.test.tuple;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.fuelteam.watt.lucky.tuple.Tuple;
+import org.fuelteam.watt.lucky.tuple.TupleType;
+import org.fuelteam.watt.lucky.utils.Vardump;
+import org.junit.Test;
+
+public class DefaultTupleTest {
+
+    public static TupleType tripletTupleType = TupleType.DefaultFactory.create(Number.class, String.class, Character.class);
+    
+    public static TupleType quadruple  = TupleType.DefaultFactory.create(4);
+    
+    public static TupleType emptyTupleType = TupleType.DefaultFactory.create();
+
+    @Test
+    public void one() {
+        final Tuple t1 = tripletTupleType.of(1, "one", 'a');
+        Vardump.print("t1 = " + t1);
+    }
+
+    @Test
+    public void two() {
+        final Tuple t2 = tripletTupleType.of(2l, "two", 'b');
+        Vardump.print("t2 = " + t2);
+    }
+
+    @Test
+    public void three() {
+        final Tuple t3 = tripletTupleType.of(3f, "three", 'c');
+        Vardump.print("t3 = " + t3);
+    }
+    
+    @Test
+    public void four() {
+        final Tuple t4 = quadruple.of(3f, "three", 'c', 1d);
+        Vardump.print("t4 = " + t4);
+    }
+
+    @Test
+    public void nullTuple() {
+        final Tuple tnull = tripletTupleType.of(null, "(null)", null);
+        Vardump.print("tnull = " + tnull);
+    }
+
+    @Test
+    public void empty() {
+        final Tuple tempty = emptyTupleType.of();
+        Vardump.print("tempty = " + tempty);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wrongTypes() {
+        tripletTupleType.of(1, 2, 3);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wrongNumberOfArguments() {
+        emptyTupleType.of(1);
+    }
+
+    @Test
+    public void valueType() {
+        final Tuple t9 = tripletTupleType.of(9, "nine", 'i');
+        assertEquals(Integer.class, t9.getNthValue(0).getClass());
+    }
+}
